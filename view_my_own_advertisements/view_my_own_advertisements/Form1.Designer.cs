63275fcfc983c14e425832534178cd5b64911b65
skipbox <skipbox500@gmail.com>
2019-02-15 07:21:33 +0800

    Added the settings form to the project added random display of images

---
+++ view_my_own_advertisements/view_my_own_advertisements/Form1.Designer.cs
@@ -0,0 +1,341 @@
+namespace view_my_own_advertisements
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.set_height = new System.Windows.Forms.Button();
+            this.set_width = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.width_b = new System.Windows.Forms.Button();
+            this.image_youtube = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.but_settings = new System.Windows.Forms.Button();
+            this.pic_box_main = new System.Windows.Forms.PictureBox();
+            this.but_list_files = new System.Windows.Forms.Button();
+            this.but_rnd = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.but_refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image_youtube)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_box_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(835, 273);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 31);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Autojacker";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(833, 311);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "student jobs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(833, 347);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "student jobs";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // set_height
+            // 
+            this.set_height.Location = new System.Drawing.Point(849, 407);
+            this.set_height.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.set_height.Name = "set_height";
+            this.set_height.Size = new System.Drawing.Size(64, 46);
+            this.set_height.TabIndex = 9;
+            this.set_height.Text = "set height";
+            this.set_height.UseVisualStyleBackColor = true;
+            this.set_height.Click += new System.EventHandler(this.set_height_Click);
+            // 
+            // set_width
+            // 
+            this.set_width.Location = new System.Drawing.Point(849, 482);
+            this.set_width.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.set_width.Name = "set_width";
+            this.set_width.Size = new System.Drawing.Size(64, 46);
+            this.set_width.TabIndex = 13;
+            this.set_width.Text = "set_width";
+            this.set_width.UseVisualStyleBackColor = true;
+            this.set_width.Click += new System.EventHandler(this.set_width_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(849, 534);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 46);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "snap doc right";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(859, 586);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(64, 46);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "snap doc top";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::view_my_own_advertisements.Properties.Resources.blueetooth_2;
+            this.pictureBox1.Location = new System.Drawing.Point(396, 7);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(105, 2);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 46);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "530";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // width_b
+            // 
+            this.width_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.width_b.Location = new System.Drawing.Point(200, 2);
+            this.width_b.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.width_b.Name = "width_b";
+            this.width_b.Size = new System.Drawing.Size(95, 46);
+            this.width_b.TabIndex = 18;
+            this.width_b.Text = "710";
+            this.width_b.UseVisualStyleBackColor = true;
+            this.width_b.Click += new System.EventHandler(this.width_b_Click);
+            // 
+            // image_youtube
+            // 
+            this.image_youtube.Image = ((System.Drawing.Image)(resources.GetObject("image_youtube.Image")));
+            this.image_youtube.Location = new System.Drawing.Point(491, 2);
+            this.image_youtube.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.image_youtube.Name = "image_youtube";
+            this.image_youtube.Size = new System.Drawing.Size(96, 82);
+            this.image_youtube.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image_youtube.TabIndex = 19;
+            this.image_youtube.TabStop = false;
+            this.image_youtube.Click += new System.EventHandler(this.image_youtube_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(11, 2);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(95, 46);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "500";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(295, 2);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(95, 46);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "610";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // but_settings
+            // 
+            this.but_settings.Location = new System.Drawing.Point(12, 55);
+            this.but_settings.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_settings.Name = "but_settings";
+            this.but_settings.Size = new System.Drawing.Size(95, 34);
+            this.but_settings.TabIndex = 24;
+            this.but_settings.Text = "Settings";
+            this.but_settings.UseVisualStyleBackColor = true;
+            this.but_settings.Click += new System.EventHandler(this.but_settings_Click);
+            // 
+            // pic_box_main
+            // 
+            this.pic_box_main.Image = ((System.Drawing.Image)(resources.GetObject("pic_box_main.Image")));
+            this.pic_box_main.Location = new System.Drawing.Point(11, 100);
+            this.pic_box_main.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pic_box_main.Name = "pic_box_main";
+            this.pic_box_main.Size = new System.Drawing.Size(739, 625);
+            this.pic_box_main.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_box_main.TabIndex = 25;
+            this.pic_box_main.TabStop = false;
+            // 
+            // but_list_files
+            // 
+            this.but_list_files.Location = new System.Drawing.Point(113, 57);
+            this.but_list_files.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_list_files.Name = "but_list_files";
+            this.but_list_files.Size = new System.Drawing.Size(95, 41);
+            this.but_list_files.TabIndex = 26;
+            this.but_list_files.Text = "list files";
+            this.but_list_files.UseVisualStyleBackColor = true;
+            this.but_list_files.Click += new System.EventHandler(this.but_list_files_Click);
+            // 
+            // but_rnd
+            // 
+            this.but_rnd.Location = new System.Drawing.Point(236, 55);
+            this.but_rnd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_rnd.Name = "but_rnd";
+            this.but_rnd.Size = new System.Drawing.Size(155, 28);
+            this.but_rnd.TabIndex = 27;
+            this.but_rnd.Text = "Random";
+            this.but_rnd.UseVisualStyleBackColor = true;
+            this.but_rnd.Click += new System.EventHandler(this.but_rnd_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(828, 103);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(177, 153);
+            this.webBrowser1.TabIndex = 28;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(613, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 17);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "use zoom it will make every image fit a little counter-intative";
+            // 
+            // but_refresh
+            // 
+            this.but_refresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but_refresh.ForeColor = System.Drawing.Color.Green;
+            this.but_refresh.Location = new System.Drawing.Point(617, 51);
+            this.but_refresh.Name = "but_refresh";
+            this.but_refresh.Size = new System.Drawing.Size(122, 31);
+            this.but_refresh.TabIndex = 30;
+            this.but_refresh.Text = "Refresh";
+            this.but_refresh.UseVisualStyleBackColor = true;
+            this.but_refresh.Click += new System.EventHandler(this.but_refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1243, 754);
+            this.Controls.Add(this.but_refresh);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.but_rnd);
+            this.Controls.Add(this.but_list_files);
+            this.Controls.Add(this.pic_box_main);
+            this.Controls.Add(this.but_settings);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.image_youtube);
+            this.Controls.Add(this.width_b);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.set_width);
+            this.Controls.Add(this.set_height);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "My own advertisements";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image_youtube)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_box_main)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button set_height;
+        private System.Windows.Forms.Button set_width;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button width_b;
+        private System.Windows.Forms.PictureBox image_youtube;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button but_settings;
+        private System.Windows.Forms.PictureBox pic_box_main;
+        private System.Windows.Forms.Button but_list_files;
+        private System.Windows.Forms.Button but_rnd;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button but_refresh;
+    }
+}
+
